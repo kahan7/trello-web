907ff9e491f837557ce4d84d67b6c126e3afb564
kahan7 <nguyenminhkhang234@gmail.com>
2024-10-19 14:10:26 +0700

    change theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Typography from '@mui/material/Typography'
 function App() {
 
   return (
     <>
       <div>
+        <Typography variant='body2' color="text.secondary">Text Typograpphy</Typography>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
